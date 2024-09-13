2f298cf508b2702403dc85f6b1a3a35b6a7317e2
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-09-13 18:42:38 +0300

    fix #1

--- spacex_launches.py
+++ spacex_launches.py
@@ -36,11 +36,5 @@ if __name__ == '__main__':
     launch_id = None
     if args.launch_id:
         launch_id = args.launch_id
-    filestream.get_filename_from_url(fetch_launch(launch_id)["links"][
-        "flickr"
-        ][
-        "original"
-        ][
-            0
-        ]
-    )
+    filestream.get_filename_from_url(fetch_launch(launch_id)
+                                     ["links"]["flickr"]["original"][0])
