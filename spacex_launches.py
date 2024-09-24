33188a4edd02312ce7a3a60aa716d4150e6ce397
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-09-24 18:25:44 +0300

    made the code work

--- spacex_launches.py
+++ spacex_launches.py
@@ -1,6 +1,8 @@
 import argparse
 import filestream
 import requests
+import os
+from dotenv import load_dotenv
 
 
 def fetch_launch(launch_id):
@@ -13,6 +15,7 @@ def fetch_launch(launch_id):
 
 
 if __name__ == '__main__':
+    load_dotenv()
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--launch_id", "-id",
@@ -24,6 +27,9 @@ if __name__ == '__main__':
     launch_id = args.launch_id
     fetched_launch = fetch_launch(launch_id)
     images_list = []
-    for v in fetched_launch["links"]["flickr"]["original"]:
+    for i, v in enumerate(fetched_launch["links"]["flickr"]["original"]):
         filename = filestream.get_filename_from_url(v)
+        fileext = filestream.get_file_extension(v)
+        file_path = f"{os.environ["IMAGES_DIRECTORY"]}/{i}" # selfaware here too
+        filestream.download_image() # i forgot to add this, thankfully was a pre-release lol
         images_list.append(filename)
