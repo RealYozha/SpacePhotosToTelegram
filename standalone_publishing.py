01f8ba967ae0a6bba5dc7df53567f83fef4a42e4
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-10-15 18:50:41 +0300

    updated standalone publishing

--- standalone_publishing.py
+++ standalone_publishing.py
@@ -14,12 +14,20 @@ def run_standalone_bot(bot: Bot, wait_mins: int) -> None:
     wait_secs = wait_mins / 60
     while True:
         if not is_enabled():
+            print("[WARN] Standalone Publishing has been disabled. The script's going to close.")
+            sleep(2)
+            print("[EXIT] Ending Script.")
+            exit()
             break
         images = shuffle(os.walk(os.environ["IMAGES_DIRECTORY"])[2])
         for image in images:
             if not is_enabled():
+                print("[WARN] Standalone Publishing has been disabled. The script's going to close.")
+                sleep(2)
+                print("[EXIT] Ending Script.")
+                exit()
                 break
-            bot.telegram_bot.send_document(image)
+            bot.publish_photo(os.environ["TELEGRAM_CHAT_ID"], image)
             sleep(wait_secs)
         sleep(wait_secs)
 
@@ -33,12 +41,18 @@ if __name__ == '__main__':
         default=1
     )
     args = parser.parse_args()
+    if not is_enabled():
+        print("[WARN] Standalone Publishing is disabled. Did you forget to enable it in the .env file? The script's going to close.")
+        sleep(2)
+        print("[EXIT] Ending Script.")
+        exit()
     bot = None
     if args.tg_bot_token:
         bot = telegram_shorthands.Bot(args.tg_bot_token)
     else:
-        print("[ ERR! ] - Telegram Bot Token Necessary")
-        print("[ Exiting ] - Ending Script")
+        print("[ERR!] Telegram Bot Token not provided. The script's going to close.")
+        sleep(2)
+        print("[EXIT] Ending Script.")
         exit()
     remake_image_directory()
     run_standalone_bot(bot,
