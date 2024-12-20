a2335e5bd2644806d8119f97add1ffcc0147ac7d
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-12-20 17:44:01 +0300

    c19

--- webstream.py
+++ webstream.py
@@ -13,22 +13,19 @@ def get_http(
     json=None,
     headers=None,
     auth=None,
-    max_attempts: int = 1,
-    tickrate: int = 1,
+    max_attempts: int=1,
+    tickrate: int=1,
 ):
-    if url:
-        response = None
-        for attempt in range(max_attempts):
-            print(f"[Web:Info] HTTP GET {url} (attempt {attempt})")
-            if not response:
-                response = requests.get(
-                    url=url, params=params, json=json, headers=headers, auth=auth
-                )
-            if response:
-                response.raise_for_status()
-                return response
-            sleep(1 / tickrate)  # zZ
+    response = None
+    for attempt in range(max_attempts):
+        print(f"[Web:Info] HTTP GET {url} (attempt {attempt})")
         if not response:
-            print(f"[ERROR] Couldn't HTTP GET ({url})!")
-    else:
-        print("[ERROR] No URL!")
+            response = requests.get(
+                url=url, params=params, json=json, headers=headers, auth=auth
+            )
+        if response:
+            response.raise_for_status()
+            return response
+            sleep(1 / tickrate)  # zZ
+    if not response:
+        print(f"[ERROR] Couldn't HTTP GET ({url})!")
